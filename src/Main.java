86131ccbaab65c7440302736e3d0f9aed379c2d1
Anatoly <anatoly8855@gmail.com>
2019-11-24 15:44:57 +0200

    Домашняя работа задание 1

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Как я провел лето");
+        System.out.println("\nЛето было жарким\n\tЛето было очень жарким\n\t\tНо в принципе все было хорошо");
+        System.out.println();
+    }
+}
+
+//  Домашняя работа  задание 1
+
+
