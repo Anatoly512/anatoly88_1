4fbb6a1f2b407d66d8d01b6debe9da44379b0fad
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-08 15:42:16 +0200

    Modified .gitignore

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("\nЛето было жарким\n\tЛето было очень жарким\n\t\tНо в принципе все было хорошо");
         System.out.println();
     }
+
 }
 
 //  Домашняя работа  задание 1
