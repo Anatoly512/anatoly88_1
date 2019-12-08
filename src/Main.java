e440c9a0c69275acccce6bf1828848db8a64dca6
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-08 17:37:14 +0200

    New (and working) structure of programm.

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Как я провел лето");
-        System.out.println("\nЛето было жарким\n\tЛето было очень жарким\n\t\tНо в принципе все было хорошо");
-        System.out.println();
-
         String name = new String();
         int number = 0;
 
-        System.out.println("What is your name? ");
-        name = stringInput();
-        if (name.equals("A"))  {
-            System.out.println("Hello, " + name);
-        } else{
-            System.out.println("else " + name);
-        }
+        System.out.println("What task do you want to run (1, 2 or 3) ? ");
+        choice();
+    }
+
+        public static void choice () {
+            int number;
+            number = numberInput();
+            if (number == 1) {
+                Task1();
+            }
+            if (number == 2) {
+                Task2();
+            }
+            if (number == 3) {
+                Task3();
+            } else {
+                System.out.println("Please, try again (possible variants: 1, 2 or 3) : ");
+                choice();
+            }
+
+    }
+
+    public static void Task1 () {
+        System.out.println("Как я провел лето");
+        System.out.println("\nЛето было жарким\n\tЛето было очень жарким\n\t\tНо в принципе все было хорошо");
+        System.out.println();
+    }
+
+    public static void Task2 () {
+
+    }
+
+    public static void Task3 () {
 
     }
 
@@ -34,6 +56,6 @@ public class Main {
 
 }
 
-//  Домашняя работа  задание 1
+//  Домашняя работа  задание 1, 2 and 3
 
 
