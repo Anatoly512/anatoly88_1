e6a46cafb594ebf7251bfc4127dd3bea23a6cfc3
Anatoly512 <56732684+Anatoly512@users.noreply.github.com>
2019-12-08 16:25:40 +0200

    before changes in Main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Как я провел лето");
         System.out.println("\nЛето было жарким\n\tЛето было очень жарким\n\t\tНо в принципе все было хорошо");
         System.out.println();
+
+        String name = new String();
+        stringInput();
+
+
+    }
+
+    public static void stringInput () {
+
+            Scanner input = new Scanner(System.in);
+            System.out.println("What is your name? ");
+            String name = input.next();
+            if (name == "A")  {
+                System.out.println("Hello, " + name);
+            } else{
+            System.out.println("else " + name);
+        }
     }
 
 }
